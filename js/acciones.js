7088a9604fb333af79b339da97d3d52abfe0574b
luz maria <corazondehielo.luz@hotmail.com>
2018-05-10 10:32:41 -0500

    5

--- js/acciones.js
+++ js/acciones.js
@@ -3,9 +3,9 @@ $(document).ready(function(e){
 	document.addEventListener("deviceready",function(){
 		$('#sonar').tap(function(){
 			navigator.notification.beep(2);
-		});//cierre del beep
-		$('#vibrar1').tap(function(){
+		}); //cierre del beep
+		$('#vibrar').tap(function(){
 			navigator.notification.vibrate(2000);
-		});//cierre del vibrate
-	},false);//cierra el deviceready
-});//cierra document ready
+		}); //cierre del vibrate
+	},false); //cierra el deviceready
+}); //cierra document ready
